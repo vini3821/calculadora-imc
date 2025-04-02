1e9fa349efb14826439028734e88abc3ff94ed87
vini3821 <vini6622@gmail.com>
2025-04-01 22:14:37 -0300

    final da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
+import { Box, Card, Typography } from '@mui/material';
+
 function App() {
 
 
   return (
-    <>
-      <div>
-        <p >
-          Teste
-        </p>
-      </div>
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'flex-start'}
+      width={'100vw'}
+      height={'100vh'}
+      
+    >
+      <Card
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
+          marginTop: '144px',
+          marginX:'16px'
 
-    </>
+        }}
+      >
+        {/*Titulo */}
+        <Box
+          marginY={'16px'}
+        >
+          <Typography variant='h5' fontWeight={"700"}>
+            Calculadora IMC
+          </Typography>
+        </Box>
+        {/*Entradas */}
+
+        <Box>
+          entradas
+        </Box>
+
+        {/*Resultado */}
+
+        <Box>
+          resultado
+        </Box>
+
+      </Card>
+    </Box>
+
   )
 }
 
